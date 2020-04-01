1a44eee4c996ba4fe0ba7675fc1fe7bf035d27eb
Ashley <adoel3507@gmail.com>
2020-04-01 17:29:20 +0100

    Fixed issue with powered by height, now in flexbox with canon logo

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,15 @@
-// login
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+(function () {// login
+  const loginForm = document.querySelector('#login-form');
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    window.location.replace("main.html");
-  });
-});
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+      window.location.replace("main.html");
+    });
+  })
+})();
